14e605f0664707dee33bbb3400cd43dac648fe85
Aura Román <kyradiscord@gmail.com>
2023-04-13 00:50:16 +0200

    feat: initial commit (#4)
    Co-authored-by: Jeroen Claassens <jeroen.claassens@live.nl>

---
+++ src/lib/utilities/ring.ts
@@ -0,0 +1,19 @@
+import { envParseString } from '@skyra/env-utilities';
+import { Json, safeTimedFetch } from '@skyra/safe-fetch';
+
+const InternalRingUrl = envParseString('INTERNAL_RING_URL');
+const InternalRingHeaders = {
+	authorization: envParseString('INTERNAL_RING_TOKEN'),
+	'content-type': 'application/json'
+};
+
+export function fetchLimits(guildId: string | bigint) {
+	return Json<RingLimits>(safeTimedFetch(`${InternalRingUrl}/${guildId}`, 1000, { headers: InternalRingHeaders }));
+}
+
+export const DefaultLimits = { maximumYouTubeSubscriptions: 3, maximumTwitchSubscriptions: 5 } as const satisfies RingLimits;
+
+export interface RingLimits {
+	maximumYouTubeSubscriptions: number;
+	maximumTwitchSubscriptions: number;
+}
